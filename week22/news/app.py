3aa0c5889e97a1c223ff3e4f49319adef3b239cc
yangxianxu <295907629@qq.com>
2018-04-26 14:49:27 +0800

    fix #1 database version

---
+++ week22/news/app.py
@@ -0,0 +1,60 @@
+from flask import Flask,render_template
+import json
+import os
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/app'
+db = SQLAlchemy(app)
+
+class File(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    created_time = db.Column(db.DateTime)
+    content = db.Column(db.Text)
+
+    category_id = db.Column(db.Integer,db.ForeignKey('category.id')
+    #category = db.relationship('Category', backref=db.backref('files', lazy='dynamic'))
+
+    #def __init__(self, title, category, content):
+    def __init__(self, title, content):
+        self.title = title
+        created_time = datetime.utcnow()
+        self.category = category
+        self.content = content
+
+    def __repr__(self):
+        return '<File %r>' % self.title
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return '<Category %r>' % self.name
+
+@app.route('/')
+def index():
+    target_dir = '/home/shiyanlou/files/'
+    articles = File.query.all()
+    return render_template('index.html',articles=articles)
+
+
+@app.route('/files/<file_id>')
+def file(file_id):
+    article = File.query.filter_by(id=file_id).first()
+    if not article:
+        abort(404)
+    else:
+        return render_template('file.html',article=article)
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
+
+if __name__ == '__main__':
+    app.run()
